ffc686192def193c04ced12dbf19f0f13d62a685
Yuanfang Xia <johnnyrxia4@126.com>
2016-03-07 15:01:51 +0800

    Final version

---
+++ i18n/en-US/dict.js
@@ -0,0 +1,41 @@
+module.exports = {
+    experience_type: {
+        "internship": "(internship)"
+    },
+    skill_type: {
+        "dw": "BI/Data Warehouse/Data Mining",
+        "frontend": "Frontend Development",
+        "backend": "Backend Development",
+        "algo": "Machine Learning Practice",
+        "miscellaneous": "Miscellaneous"
+            },
+    education_type: {
+        "undergraduate": "(Bachelor's degree)",
+        "graduate": "(Master's degree)"
+    },
+    link_type: {
+        "zhihu": "Zhihu",
+        "doubanbook": "Douban",
+        "repo": "Source",
+        "pdf": "PDF"
+    },
+    section_names: {
+        "education": {
+            "ch": "Education",
+            "en": "Education"
+        },
+        "language": {
+            "ch": "Language",
+            "en": "Language"
+        },
+        "experience": {
+            "ch": "Experience",
+            "en": "Experience"
+        },
+        "skills": {
+            "ch": "Skills",
+            "en": "Skills"
+        }
+    },
+    lt_ie8_comment: "You are using IE8 or lower version of IE, which is not supported."
+};
